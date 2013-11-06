4a2ad7717b5ac5cefc135ccbd7a62b363614ec37
Carla Ares <carla.ares@gmail.com>
2013-11-05 21:08:44 -0300

    setting heroku url for device mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,6 @@ Delivery::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => 'fast-depths-6041.herokuapp.com' }
+
 end
